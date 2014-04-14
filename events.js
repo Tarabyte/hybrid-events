d4f27001fe2485287b48cd37bf3deef30715ff22
Yury Tarabanko <tarabanko@mirapolis.ru>
2014-04-14 23:23:19 +0400

    setup/teardown logic

--- events.js
+++ events.js
@@ -1,1 +1,158 @@
-
+/*global window*/
+~function(win) {
+    /**
+     * @class PubSub implementation.
+     */
+    var Hub = function(ctx) {
+            this.$events = {};
+            this.ctx = ctx;
+        },
+        
+        key = 'HybridEventHub' + Date.now(),
+        
+        getHub = function(ctx) {
+            return ctx[key] || (ctx[key] = new Hub(ctx));
+        },
+        hubProto = Hub.prototype,
+        
+        /**
+         * @class Storage for event callbacks.
+         * Contains custom options.
+         */
+        Emitter = function(name, options) {
+            this.$callbacks = [];
+            this.$options = options || {};
+            this.$name = name;
+        },
+        
+        emitterProto = Emitter.prototype;
+    
+    /**
+     * Add callback to the storage.
+     * Run setup/add logic.
+     */
+    emitterProto.add = function(callback, ctx) {
+        var emitter = this,
+            callbacks = emitter.$callbacks,
+            options = emitter.$options,
+            setup = options.setup,
+            add = options.add,
+            name = emitter.$name,
+            callbacksNumber = callbacks.length;
+        
+        callbacks.push(callback);
+        
+        if(setup && !callbacksNumber) { //There were no callbacks before
+            setup.call(ctx, name);    
+        }
+        if(add) {
+            add.call(ctx, name);
+        }
+        
+        return function unbinder() {
+            emitter.remove(callback, ctx);    
+        };
+    };
+    
+    /**
+     * Add or remove callback.
+     */
+    emitterProto.toggle = function(callback, ctx) {
+        return this[~this.$callbacks.indexOf(callback) ?'remove' : 'add'](callback, ctx);
+    };
+    
+    /**
+     * Remove callback from the storage.
+     * Run remove/teardown logic.
+     */
+    emitterProto.remove = function(callback, ctx) {
+        var callbacks = this.$callbacks,
+            name = this.$name,
+            position = callbacks.indexOf(callback),
+            options = this.$options,
+            remove = options.remove,
+            teardown = options.teardown;
+        
+        if(~position) { //callback is in there
+            callbacks.splice(position, 1);
+            
+            if(remove) { 
+                remove.call(ctx, name);
+            }
+            if(teardown && !callbacks.length) {
+                teardown.call(ctx, name);    
+            }
+        }                
+    };
+    
+    /**
+     * Run callbacks.
+     */
+    emitterProto.fire = function(event) {
+        var callbacks = this.$callbacks;
+        
+        if(callbacks.length) {
+            callbacks = callbacks.slice(0); //create a copy;
+            
+            callbacks.forEach(function(cb) {
+                cb.call(event.target, event);    
+            });    
+        }
+    };
+    
+    /**
+     * Bind|unbind callback for event.
+     * * Bind callback if is not already binded.
+     * * Unbind otherwise.
+     * @param {String} name Event name.
+     * @param {Function} callback Function to be called.
+     * @param {Object} options Additional event params.
+     * @returns {Function} Unbinder function.
+     */
+    hubProto.sub = function(name, callback, options) {
+        var event = this.$events[name];
+        
+        if(!event) {
+            this.$events[name] = event = new Emitter(name, options);    
+        }
+        
+        event.toggle(callback, this.ctx);
+    };
+    
+    /**
+     * Publish event.
+     * @param {String} name - Event name.
+     * @param {Event} event - Event containing data, context etc
+     */
+    hubProto.pub = function(name, event) {
+        this.$events[name].fire(event);
+    };
+    
+    /**
+     * @param {String} name - Event name, required.
+     * @param {Object} options - Additional event options. Optional 
+     * @returns {Function} Hybrid event function.
+     */
+    function event(name, options) {
+        if(!name) {
+            throw new Error('Event name is required');
+        }
+        
+        return function(arg) {
+            var hub = getHub(this);
+            
+            if(typeof arg == 'function') { //bind|unbind mode
+                return hub.sub(name, arg, options);        
+            }
+            else { //trigger mode
+                hub.pub(name, {
+                    target: this,
+                    data: arg
+                });    
+            }
+        };
+    }
+    
+    win.HybridEvent = event; 
+    
+}(window);
--- test/events_test.js
+++ test/events_test.js
@@ -1,1 +1,131 @@
+module('Hybrid events');
+
+test('HybridEvent is defined', function() {
+    ok(HybridEvent, 'is defined');
+    equal(typeof HybridEvent, 'function', 'and is a function');
+});
+
+test('Event creation', function() {
+    expect(9);
+    throws(function() {
+        var myEvent = HybridEvent();
+    }, 'should throw without name');
+    
+    var myEvent = HybridEvent('some');
+    
+    ok(myEvent, 'event is created');
+    
+    equal(typeof myEvent, 'function', 'event is actually a function');
+    
+    var holder = {event: myEvent},
+        called = 0,
+        callback = function(ev) {
+            if(!called) {
+                called = 1;
+                ok(true, 'called');
+                ok(ev, 'got event');
+                equal(this, holder, 'this is a holder');
+                equal(ev.data, data, 'ev.data is specified');
+                equal(ev.target, holder, 'ev.target set to be a holder');
+            } 
+            else {
+                ok(false, 'should not be called twice');
+            }
+        };
+    
+    var unbinder = holder.event(callback);
+    equal(typeof unbinder, 'function', 'got another function back');
+    
+    var data = {some: 'data'};
+    
+    holder.event(data);
+    
+    unbinder();
+    
+    holder.event(data);
+    holder.event(data);
+    
+});
+
+test('multiple events', function() {
+    expect(2);
+    var holder = {itHappens: HybridEvent('itHappens')},
+        cb1 = function bindsCb3unbindsCb2() {
+            ok(true, 'called');
+            holder.itHappens(cb3); //should not be called this time
+            holder.itHappens(cb2); //should be called this time
+        },
+        cb2 = function shouldBeCalled() {
+            ok(true, 'cb2 should be called');
+        },
+        cb3 = function shouldNeverBeCalled() {
+            ok(false, 'cb3 should not called');    
+        };
+    
+    holder.itHappens(cb1);
+    holder.itHappens(cb2);
+    
+    holder.itHappens();     
+});
+
+test('custom options', function() {
+    expect(10);
+    var called = {
+            add: 0,
+            remove: 0
+        },
+        options = {
+            setup: function() {
+                if(called.setup) {
+                    ok(false, 'setup should only be called once');
+                }
+                else {
+                    called.setup = 1;
+                    ok(true, 'setup called');
+                }
+            },
+            
+            add: function() {
+                called.add = called.add || 0;
+                called.add++;
+                ok(true, 'add called');
+            },
+            
+            teardown: function() {
+                if(called.teardown) {
+                    ok(false, 'teardown should only be called once');
+                }
+                else {
+                    called.teardown = 1;
+                    ok(true, 'teardown called');
+                }
+            },
+            remove: function() {
+                called.remove++;
+                ok(true, 'remove called');
+            }
+        },
+        holder = {
+            some: HybridEvent("some", options)
+        };
+    
+    var cb1 = function(){
+        ok(true, 'cb1 called');
+    },
+        cb2 = function() {
+            ok(true, 'cb2 called');    
+        };
+    
+    var unbinder1 = holder.some(cb1),
+        unbinder2 = holder.some(cb2);
+    
+    holder.some();
+    
+    equal(called.add, 2, 'add was called 2 times');
+    
+    unbinder1();
+    holder.some(cb2);
+    equal(called.remove, 2, 'remove was called 2 times');
+    
+});
 
